c62c3e1adfc55592589b0c71e58b28968a130dc6
Stanisof <128597372+Stanisof@users.noreply.github.com>
2023-08-16 19:37:48 +0300

    Add clear button and few design changes

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#canvas');
 
+const slider = document.querySelector('#slider');
+slider.oninput = function() {
+   size = slider.value;
+   let pixelSize = (800 / size) +"px";
+   let boxCount = slider.value * slider.value;
+   console.log(pixelSize);
+   return pixelSize;
+}
 
 
 function createBox(){
+    
     let box = document.createElement('div');
     box.classList.add('box');
-    box.style.flex = '1 1 60px'
+    box.style.height= '25px';
+    box.style.width= '25px';
+
     box.addEventListener('mouseover', () => {
         box.style.backgroundColor = 'black';
     });
-   
 
     container.appendChild(box);
+
+    const clear = document.querySelector('#clear');
+    clear.addEventListener('click', () => {
+    box.style.backgroundColor = 'white';
+})
 }
 
-for (let i = 0; i < 256; i++) {
+
+
+for (let i = 0; i < 1024; i++) {
     createBox()
 }
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    
+})
